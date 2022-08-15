1921dbe303496c272cead9fd0c4426938a4d86e0
AlanRo <alekroga@gmail.com>
2022-08-15 21:15:57 +0500

    Auth page as seperate page

---
+++ src/auth/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <div id="app">
+    <form class="auth-form">
+      <div>
+        <label for="name">Name</label>
+        <base-input id="name" size="large" />
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <base-input
+          id="password"
+          size="large"
+          :input-props="{ type: 'password' }"
+        />
+      </div>
+      <div>
+        <base-button size="large">Log In</base-button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+import { BaseInput, BaseButton } from "@/components/base";
+
+export default {
+  name: "App",
+  components: { BaseInput, BaseButton },
+  data() {
+    return {};
+  },
+  computed: {},
+  created() {},
+  methods: {},
+};
+</script>
+
+<style lang="scss">
+.app {
+  width: 100%;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  background-color: green;
+}
+.auth-form {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  border-radius: 10px;
+  background-color: white;
+}
+</style>
--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,33 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  pages: {
+    index: {
+      // entry for the page
+      entry: "src/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist/index.html
+      filename: "index.html",
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "Web Chat",
+    },
+    auth: {
+      // entry for the page
+      entry: "src/auth/main.js",
+      // the source template
+      template: "public/index.html",
+      // output as dist/index.html
+      filename: "auth.html",
+      // when using title option,
+      // template title tag needs to be <title><%= htmlWebpackPlugin.options.title %></title>
+      title: "Web Chat - Authorize",
+    },
+  },
+  configureWebpack: {
+    mode: "development",
+  },
   /** SCSS assets **/
   css: {
     loaderOptions: {
